08cfa880653da2440b8e30881874d29f5c223070
evan <evan.kusyanto@students.amikom.ac.id>
2023-08-06 17:33:45 +0700

    update

--- src/components/Home/Congrate.jsx
+++ src/components/Home/Congrate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import congrate from "../../image/congrate.jpeg";
 const Congrate = () => {
   return (
-    <div className="w-full h-[90vh] justify-center md:h-[70vh] bg-gradient flex flex-col md:flex-row layout items-center">
+    <div className="w-full h-[90vh] justify-center md:h-[80vh] bg-gradient flex flex-col md:flex-row layout items-center">
       <div className="flex flex-col gap-y-2 mb-4 md:gap-y-14">
         <h1 className="underline text-[30px]  md:leading-[36px] text-white  ">
           Congratulation
@@ -13,7 +13,7 @@ const Congrate = () => {
         </p>
       </div>
       <div className="border-[#f59e0b] p-2 border-2  rounded-lg ">
-        <img src={congrate} alt="" className=" w-[800px] rounded-lg" />
+        <img src={congrate} alt="" className=" w-[700px] rounded-lg" />
       </div>
     </div>
   );
